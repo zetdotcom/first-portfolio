b7567eec9bee2aa597bbed6554c4bad86decf9ea
Michal Zet <michal.zadarnowski@gmail.com>
2018-02-13 00:09:36 +0000

    desc changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link';
 const Header = () => (
 	<div
 		style={{
-			background: 'rebeccapurple',
+			background: 'darkblue',
 			marginBottom: '1.45rem'
 		}}
 	>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link';
 
 const IndexPage = () => (
 	<div>
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
+		<h1>Michal Zadarnowski</h1>
+		<h3>Front End/Web Apps/ Javascript Developer</h3>
 		<Link to="/page-2/">Go to next page</Link>
 	</div>
 );
