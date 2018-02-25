0f8c66b59f775f6cb168a5f259fff66dd28b9101
Michal Zet <michal.zadarnowski@gmail.com>
2018-02-25 11:56:43 +0000

    projects section styling-skills

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,43 @@ import Link from 'gatsby-link';
 import './Projects.scss';
 import ProjectsList from '../../json/projects.json';
 
+const project = () => {
+		return (ProjectsList.map((item, key) => {
+				const checkRepo = () => {
+						if (item.git === "") {
+								return <button >Private repo</button>
+						} else {
+								return <a href={item.git} target="blank">
+										<button >Git repo</button>
+								</a>
+						}
+				};
+				const skillso = () => {
+						return (item.skills.map((el) => {
+								return <div>{el}</div>
+						}))
+
+				};
+				return (
+						<div className="project" key={key}>
+								<div className="projectTitle">
+										{item.title}</div>
+								<div className="thumbnail"><img src="images/bootstrap.png"/></div>
+								<div className="projectSkills">{skillso()}</div>
+								<div className="links">
+										<a href={item.link} target="blank">
+												<button>Visti Website</button>
+										</a>
+										{checkRepo()}
+								</div>
+								<div className="description">{item.desc}</div>
+						</div>
+				);
+		}))
+};
+
 const Projects = () => {
+
 		return (
 				<div className="wrapper">
 						<div className="projects">
@@ -12,48 +48,12 @@ const Projects = () => {
 										<h1>Projects</h1>
 								</div>
 								<div className="projectsList">
-
-										{ProjectsList.map((item, key) => {
-												const checkRepo = () => {
-														if (item.git === "") {
-																return <button >Private repo</button>
-														} else {
-																return <a href={item.git} target="blank">
-																		<button >Git repo</button>
-																</a>
-														}
-												};
-												const skillso = () => {
-														item
-																.skills
-																.map((skill) => {
-																		return <div>{skill}
-																		</div>
-																})
-												};
-												return (
-														<div className="project" key={key}>
-																<div className="projectTitle">
-																		{item.title}</div>
-																<div className="thumbnail"><img src="images/bootstrap.png"/></div>
-																<div className="projectsSkills">{item.skills}</div>
-																<div className="links">
-																		<a href={item.link} target="blank">
-																				<button>Visti Website</button>
-																		</a>
-																		{checkRepo()}
-																</div>
-																<div className="description">description of project description of project
-																		description of project description of project description of project description
-																		of project.</div>
-														</div>
-												);
-										})}
-
+										{project()}
 								</div>
 						</div>
 				</div>
 		);
+
 };
 
 export default Projects;
